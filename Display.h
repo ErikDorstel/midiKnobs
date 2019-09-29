143d295ee7108366e11a16a6396081b803319212
Erik Dorstel <43003063+ErikDorstel@users.noreply.github.com>
2019-09-29 14:21:47 +0200

    Add color display support

---
+++ Display.h
@@ -0,0 +1,41 @@
+#include <ILI9341_t3.h>
+#include <font_Arial.h> // from ILI9341_t3
+#include <XPT2046_Touchscreen.h>
+#include <SPI.h>
+
+#define CS_PIN  8
+#define TFT_DC  9
+#define TFT_CS 10
+// MOSI=11, MISO=12, SCK=13
+XPT2046_Touchscreen ts(CS_PIN);
+#define TIRQ_PIN  2
+//XPT2046_Touchscreen ts(CS_PIN);  // Param 2 - NULL - No interrupts
+//XPT2046_Touchscreen ts(CS_PIN, 255);  // Param 2 - 255 - No interrupts
+//XPT2046_Touchscreen ts(CS_PIN, TIRQ_PIN);  // Param 2 - Touch IRQ Pin - interrupt enabled polling
+ILI9341_t3 tft = ILI9341_t3(TFT_CS, TFT_DC);
+
+char const *knobText[64]={"Attack","Hold","Decay","Sustain","Release","Arpeggiator Mode","Arpeggiator Speed","Glissando Speed",\
+  "VCO 1 Waveform","VCO 2 Waveform","VCO 1/2 Ratio","VCO 2 Shift","Filter Bypass","Filter Lowpass","Filter Bandpass","Filter Highpass",\
+  "LFO VCO Waveform","LFO VCO Phase Start","LFO VCO Level","LFO VCO Frequency","","","","",\
+  "LFO Filter Waveform","LFO Filter Phase Start","LFO Filter Level","LFO Filter Frequency","Filter Frequency","Filter Resonance","","VCO 1+2 Level" };
+int currScreen=0, maxScreen=3;
+
+void initScreen() {
+  tft.begin(); tft.setRotation(3); ts.begin(); ts.setRotation(1); delay(1000);
+  tft.fillScreen(ILI9341_BLACK); tft.fillRect(319-50,0,50,50,ILI9341_RED); tft.fillRect(319-50,230-50,50,50,ILI9341_RED);
+  tft.setTextColor(ILI9341_WHITE); tft.setFont(Arial_12);
+  tft.setCursor(285,17); tft.print("Up"); tft.setCursor(275,200); tft.print("Down"); }
+
+void setGauge(byte g) {
+  if (g-(g%8) == currScreen*8) { int y=2+((g%8)*30); int x=knobValues[g]*2;
+    tft.fillRect(0,y,x,26,ILI9341_BLUE); tft.fillRect(x+1,y,255-x,26,ILI9341_BLACK);
+    tft.setCursor(2,y+6); tft.print(knobText[g]); } }
+
+void setScreen() {
+  if (currScreen < 0) { currScreen=maxScreen; } if (currScreen > maxScreen) { currScreen=0; }
+  tft.setFont(Arial_12); tft.fillRect(290,110,12,12,ILI9341_BLACK); tft.setCursor(290,110); tft.print(currScreen+1); tft.setFont(Arial_10);
+  for (byte z=0;z<=7;z++) { setGauge(z+(currScreen*8)); } }
+
+void getTouch() {
+  if (ts.touched()) { TS_Point p=ts.getPoint();
+    if (p.y < 2000) { currScreen--; setScreen(); } else { currScreen++; setScreen(); } } }
--- MIDIcontroller64.ino
+++ MIDIcontroller64.ino
@@ -1,6 +1,9 @@
 #include <Metro.h>
+byte knobValues[64];
+#include "Display.h"
 
 const bool logging=false;
+const bool display=true;
 const bool hc4067_0=true;
 const bool hc4067_1=true;
 const bool hc4067_2=false;
@@ -11,27 +14,27 @@ Metro timer = Metro(10000);
 void setup() {
   if (logging) { Serial.begin(9600); }
   Serial1.begin(31250,SERIAL_8N1);
-  pinMode(2, OUTPUT); pinMode(3, OUTPUT); pinMode(4, OUTPUT); pinMode(5, OUTPUT); pinMode(13, OUTPUT);
+  pinMode(3, OUTPUT); pinMode(4, OUTPUT); pinMode(5, OUTPUT); pinMode(6, OUTPUT);
   analogReadResolution(7);
+  if (display) { initScreen(); setScreen(); }
   delay(1000); timer.interval(10); timer.reset(); }
   
 void loop() {
-  static byte knobValues[64], counter, curPort;
-  byte mulPort, knobValue;
+  static byte counter, curPort; byte mulPort, knobValue;
   if (timer.check() == 1) {
-    digitalWrite(13, LOW);
     for (mulPort=0;mulPort<=15;mulPort++) {
       selectPort(mulPort); delayMicroseconds(200);
       if (hc4067_0) { knobValue=analogRead(A0); if (knobValue!=knobValues[mulPort]) {
-        knobValues[mulPort]=knobValue; sendMidi(mulPort,knobValue); digitalWrite(13, HIGH); } }
+        knobValues[mulPort]=knobValue; sendMidi(mulPort,knobValue); } }
       if (hc4067_1) { knobValue=analogRead(A1); if (knobValue!=knobValues[mulPort+16]) {
-        knobValues[mulPort+16]=knobValue; sendMidi(mulPort+16,knobValue); digitalWrite(13, HIGH); } }
+        knobValues[mulPort+16]=knobValue; sendMidi(mulPort+16,knobValue); } }
       if (hc4067_2) { knobValue=analogRead(A2); if (knobValue!=knobValues[mulPort+32]) {
-        knobValues[mulPort+32]=knobValue; sendMidi(mulPort+32,knobValue); digitalWrite(13, HIGH); } }
+        knobValues[mulPort+32]=knobValue; sendMidi(mulPort+32,knobValue); } }
       if (hc4067_3) { knobValue=analogRead(A3); if (knobValue!=knobValues[mulPort+48]) {
-        knobValues[mulPort+48]=knobValue; sendMidi(mulPort+48,knobValue); digitalWrite(13, HIGH); } } }
+        knobValues[mulPort+48]=knobValue; sendMidi(mulPort+48,knobValue); } } }
 
   if (counter>100) { counter=0;
+    if (display) { getTouch(); }
     if (curPort>15) { curPort=0; }
     selectPort(curPort); delayMicroseconds(200);
     if (hc4067_0) { sendMidi(curPort,analogRead(A0)); }
@@ -40,8 +43,9 @@ void loop() {
     if (hc4067_3) { sendMidi(curPort+48,analogRead(A3)); }
     curPort++; } counter++; } }
 
-void selectPort(byte port) { digitalWrite(2, port&1); digitalWrite(3, port&2); digitalWrite(4, port&4); digitalWrite(5, port&8); }
+void selectPort(byte port) { digitalWrite(3, port&1); digitalWrite(4, port&2); digitalWrite(5, port&4); digitalWrite(6, port&8); }
   
 void sendMidi(byte knobNumber, byte knobValue) {
   Serial1.write(0xb0+MIDIchannel); Serial1.write(knobNumber); Serial1.write(knobValue);
+  if (display) { setGauge(knobNumber); }
   if (logging) { Serial.print(knobNumber); Serial.print(" "); Serial.println(knobValue); } }
