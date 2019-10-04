b8a3dc0a487d839a5af44e6531df96f6eade3078
Erik Dorstel <43003063+ErikDorstel@users.noreply.github.com>
2019-10-04 14:58:24 +0200

    Add files via upload

--- Display.h
+++ Display.h
@@ -10,11 +10,14 @@
 XPT2046_Touchscreen ts(Touch_CS); // XPT2046_Touchscreen ts(Touch_CS,Touch_IRQ); for interrupt enabled polling
 ILI9341_t3 tft=ILI9341_t3(TFT_CS,TFT_DC);
 
-char const *knobText[64]={"Attack","Hold","Decay","Sustain","Release","Arpeggiator Mode","Arpeggiator Speed","Glissando Speed",\
+char const *knobText[10][64]={ {"Attack","Hold","Decay","Sustain","Release","Arpeggiator Mode","Arpeggiator Speed","Glissando Speed",\
   "VCO 1 Waveform","VCO 2 Waveform","VCO 1/2 Ratio","VCO 2 Shift","Filter Bypass","Filter Lowpass","Filter Bandpass","Filter Highpass",\
   "LFO VCO Waveform","LFO VCO Phase Start","LFO VCO Level","LFO VCO Frequency","","","","",\
-  "LFO Filter Waveform","LFO Filter Phase Start","LFO Filter Level","LFO Filter Frequency","Filter Frequency","Filter Resonance","","VCO 1+2 Level" };
-int currScreen=0, maxScreen=3;
+  "LFO Filter Waveform","LFO Filter Phase Start","LFO Filter Level","LFO Filter Frequency","Filter Frequency","Filter Resonance","","VCO 1+2 Level"},\
+  {"Modulator Bottom Frequency","Modulator Top Frequency","Carrier Bottom Frequency","Carrier Top Frequency",\
+  "Envelope Speed","Envelope Limiter","Filter Resonance","Output Amplifier","Modulator Source","Carrier Source"} };
+
+int currScreen[10]={0,0}, maxScreen[10]={3,1}, currApp=0, maxApp=1;
 
 void initScreen() {
   tft.begin(); tft.setRotation(3); ts.begin(); ts.setRotation(1); delay(1000);
@@ -24,15 +27,18 @@ void initScreen() {
   tft.setCursor(275,17); tft.print("Down"); tft.setCursor(285,209); tft.print("Up"); }
 
 void setGauge(byte g) {
-  if (g-(g%8) == currScreen*8) { int y=2+((g%8)*30); int x=knobValues[g]*2;
+  if (g-(g%8) == currScreen[currApp]*8) { int y=2+((g%8)*30); int x=knobValues[g]*2;
     tft.fillRect(0,y,x,26,ILI9341_BLUE); tft.fillRect(x+1,y,255-x,26,ILI9341_BLACK);
-    tft.setCursor(4,y+7); tft.print(knobText[g]); } }
+    tft.setCursor(4,y+7); tft.print(knobText[currApp][g]); } }
 
 void setScreen() {
-  if (currScreen < 0) { currScreen=maxScreen; } if (currScreen > maxScreen) { currScreen=0; }
-  tft.setFont(Arial_12); tft.fillRect(290,112,12,12,ILI9341_BLACK); tft.setCursor(290,112); tft.print(currScreen+1); tft.setFont(Arial_10);
-  for (byte z=0;z<=7;z++) { setGauge(z+(currScreen*8)); } }
+  if (currScreen[currApp] < 0) { currScreen[currApp]=maxScreen[currApp]; } if (currScreen[currApp] > maxScreen[currApp]) { currScreen[currApp]=0; }
+  tft.setFont(Arial_12); tft.fillRect(290,112,12,12,ILI9341_BLACK); tft.setCursor(290,112); tft.print(currScreen[currApp]+1); tft.setFont(Arial_10);
+  for (byte z=0;z<=7;z++) { setGauge(z+(currScreen[currApp]*8)); } }
 
 void getTouch() {
   if (ts.touched()) { TS_Point p=ts.getPoint();
-    if (p.y < 2000) { currScreen--; setScreen(); } else { currScreen++; setScreen(); } } }
+    if ((p.y < 2000) and (p.x > 2000)) { currScreen[currApp]--; setScreen(); }
+    if ((p.y > 2000) and (p.x > 2000)) { currScreen[currApp]++; setScreen(); }
+    if ((p.y < 2000) and (p.x < 2000)) { currApp--; if (currApp<maxApp) { currApp=maxApp; } currScreen[currApp]=0; setScreen(); }
+    if ((p.y > 2000) and (p.x < 2000)) { currApp++; if (currApp>maxApp) { currApp=0; } currScreen[currApp]=0; setScreen(); } } }
